5c0bc6d1d14f1c95ef49e2d2c7c3687846746f1c
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2024-01-25 22:12:48 -0500

    end ts test setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
+  // testEnvironment: 'node',
   setupFiles: ['./jest.setup.ts']
 }
---
+++ tests/helpers/upload-file.test.ts
@@ -0,0 +1,9 @@
+import { uploadFile } from '../../src/helpers/upload-file'
+
+describe('Test in upload file', () => {
+  test('Should upload file correctly', () => {
+    const imgUrl = 'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cmFuZG9tfGVufDB8fDB8fHww';
+
+
+  })
+})
